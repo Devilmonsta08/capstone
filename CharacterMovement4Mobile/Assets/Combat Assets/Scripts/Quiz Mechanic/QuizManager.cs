34216a79af74278a0d301e392bc9ae4a32e21eac
Devilmonsta08 <97335787+Devilmonsta08@users.noreply.github.com>
2023-01-09 00:12:47 +0800

    Animation,Anchors and Button Changes
    Animations has been fixed for a moment
    Anchors and Button Changes to TMP

--- CharacterMovement4Mobile/Assets/Combat Assets/Scripts/Quiz Mechanic/QuizManager.cs
+++ CharacterMovement4Mobile/Assets/Combat Assets/Scripts/Quiz Mechanic/QuizManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 
 public class QuizManager : MonoBehaviour
 {
     public List<QuestionAndAnswer> QnA;
     public GameObject[] options;
     public int currentQuestion;
-    public Text QuestionTxt;
+    public TextMeshProUGUI QuestionTxt;
 
 
     private void Start() 
@@ -36,7 +36,7 @@ public class QuizManager : MonoBehaviour
         {
             
             options[i].GetComponent<AnswerScript>().isCorrect = false;
-            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
+            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
 
             if (QnA[currentQuestion].CorrectAnswer == i+1)
             {
