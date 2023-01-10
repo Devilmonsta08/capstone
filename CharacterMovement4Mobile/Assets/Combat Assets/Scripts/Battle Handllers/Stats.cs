c105752cfa09f5281a66dd5e51782dd851221d6c
ledjoric <88924049+ledjoric@users.noreply.github.com>
2023-01-11 03:54:09 +0800

    set enemy asset

--- CharacterMovement4Mobile/Assets/Combat Assets/Scripts/Battle Handllers/Stats.cs
+++ CharacterMovement4Mobile/Assets/Combat Assets/Scripts/Battle Handllers/Stats.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+    [SerializeField] private CharacterDatabase charDB;
+
     public string unitName;
     public int Damage;
     public int maxHP;
     public int currentHP;
+
+    private void OnEnable()
+    {
+        if(unitName == "Player") currentHP = charDB.charHealth;
+    }
+
+    private void OnDisable()
+    {
+        if (unitName == "Player") charDB.charHealth = currentHP;
+    }
 
     public bool TakeDamage(int Dmg)
     {
